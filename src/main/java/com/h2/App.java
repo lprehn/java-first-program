67bd9ef2fb298e6afe6665afd7485b505d120597
lprehn <35541263+lprehn@users.noreply.github.com>
2021-05-25 11:35:17 -0700

    Finding the Best Loan Rates

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,8 +1,7 @@
 package com.h2;
 
 /**
- * Hello world!
- *
+ * An app that completes simple mathematical calculations
  */
 public class App 
 {
@@ -11,7 +10,23 @@ public class App
         System.out.println( "Hello World!" );
     }
 
-    public static int doubleTheNumber(int number) {
-        return -1;
+    //a simple method that doubles the given number
+    public static int doubleTheNumber(int number)
+    {
+        return (number*2);
+    }
+
+    //a simple method that takes a list of numbers and
+    //returns the sum of all of the numbers in the list
+    public static int add(int[] numbers)
+    {
+        int sum = 0;
+        int length = numbers.length;
+        //loop through each item in the list
+        for (int num : numbers) {
+            //add it to the sum
+            sum += num;
+        }
+        return sum;
     }
 }
---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,56 @@
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+A class that calculates the best rates for loans
+ */
+public class BestLoanRates
+{
+    //a map of loan terms (in years) paired with the best loan
+    //rates for the given term
+    public static final Map<Integer, Float> bestRates =
+            Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //greet the user
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        //take loan term as input for rate calculation
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+        float bestRate = getRates(loanTermInYears);
+
+        //if there is no best rate for the given term,
+        //report this to the user
+        if (bestRate==0.0f)
+        {
+            System.out.println("No available rates for the term: " +
+                    loanTermInYears + " years");
+        }
+        //otherwise, report the best rate
+        else
+        {
+            System.out.println("Best Available Rate: " + bestRate + "%");
+        }
+
+        scanner.close();
+    }
+
+    //takes a loan term (in number of years) and returns the best
+    //rate for the given term, if applicable
+    public static float getRates(int loanTermInYears)
+    {
+        //if the given loan term has a corresponding rate...
+        if (bestRates.containsKey(loanTermInYears)) {
+            //return that rate
+            return bestRates.get(loanTermInYears);
+        }
+        return 0.0f;
+    }
+}
